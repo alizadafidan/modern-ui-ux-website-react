bc6cab222d03b24b797149ca2cfb793dca9a098f
Fidan Alizada <alizadafidann@gmail.com>
2022-12-22 17:30:46 +0400

    file and folder structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Navbar from "./components/navbar/Navbar";
+import Header from "./containers/header/Header";
+import Brand from "./components/brand/Brand";
+import WhatGPT3 from "./containers/whatGPT3/WhatGPT3";
+import Features from "./containers/features/Features";
+import Possibility from "./containers/possibility/Possibility";
+import CTA from "./components/cta/CTA";
+import Blog from "./containers/blog/Blog";
+import Footer from "./containers/footer/Footer";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
