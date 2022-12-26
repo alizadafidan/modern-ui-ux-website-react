e096c8a61ca72467ddebbfe142659b55408d7a75
Fidan Alizada <alizadafidann@gmail.com>
2022-12-26 15:39:25 +0400

    add brand component and style it

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import google from "../../assets/google.png";
+import slack from "../../assets/slack.png";
+import atlassian from "../../assets/atlassian.png";
+import dropbox from "../../assets/dropbox.png";
+import shopify from "../../assets/shopify.png";
 
-const Brand = () => {
-  return <div>Brand</div>;
-};
+import "./brand.css";
+
+const Brand = () => (
+  <div className="gpt3__brand section__padding">
+    <div>
+      <img src={google} alt="Google Logo" />
+    </div>
+    <div>
+      <img src={slack} alt="Slack Logo" />
+    </div>
+    <div>
+      <img src={atlassian} alt="Atlassian Logo" />
+    </div>
+    <div>
+      <img src={dropbox} alt="Dropbox Logo" />
+    </div>
+    <div>
+      <img src={shopify} alt="Shopify Logo" />
+    </div>
+  </div>
+);
 
 export default Brand;
